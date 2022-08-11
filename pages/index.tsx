bf5a107ea818689799e4b9b65adab1daa928e618
victor willian <victorwillian1012@gmail.com>
2022-08-10 22:58:21 -0300

    🦄 refactor(remove):
    remove unnecessary code

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,16 +2,7 @@ import { Flex } from "@chakra-ui/react";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-    return (
-        <Flex height="100vh" justifyContent="center" alignItems="center">
-            <Flex
-                direction="column"
-                backgroundColor="gray.700"
-                p={12}
-                rounded={6}
-            ></Flex>
-        </Flex>
-    );
+    return <div>Hello</div>;
 };
 
 export default Home;
